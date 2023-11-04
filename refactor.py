3739af3432cde3aba0f3dac0526e55b99e28b9d9
Mr Fong <75341864+fong-a@users.noreply.github.com>
2023-11-04 15:14:45 +1100

    Create refactor.py

---
+++ refactor.py
@@ -0,0 +1,38 @@
+# areas.py
+
+# This script calculates the area of a rectangle, square, and circle.
+# The code is intentionally repetitive for refactoring practice.
+
+# Calculate the area of a rectangle
+rectangle_length = 10
+rectangle_width = 5
+rectangle_area = rectangle_length * rectangle_width
+print("The area of the rectangle is:", rectangle_area)
+
+# Calculate the area of a square
+square_side = 7
+square_area = square_side * square_side
+print("The area of the square is:", square_area)
+
+# Calculate the area of a circle
+circle_radius = 6
+circle_area = 3.14159 * circle_radius * circle_radius
+print("The area of the circle is:", circle_area)
+
+# Calculate the area of another rectangle
+another_rectangle_length = 14
+another_rectangle_width = 3
+another_rectangle_area = another_rectangle_length * another_rectangle_width
+print("The area of another rectangle is:", another_rectangle_area)
+
+# Calculate the area of another square
+another_square_side = 9
+another_square_area = another_square_side * another_square_side
+print("The area of another square is:", another_square_area)
+
+# Calculate the area of another circle
+another_circle_radius = 4
+another_circle_area = 3.14159 * another_circle_radius * another_circle_radius
+print("The area of another circle is:", another_circle_area)
+
+# End of the script
